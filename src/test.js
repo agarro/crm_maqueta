edd2148b47d38997a7cb285cd912c7af817906cc
Alejandro Garro <garro.alejandro@gmail.com>
2026-03-16 13:14:38 -0300

    Initial commit of CRM ChampionSys clean source code to staging branch

---
+++ src/test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('./index'); // Ajustar si es necesario
+
+describe('Health Check API', () => {
+  it('should return 200 OK', async () => {
+    // Esto es un placeholder para el test, el index.js debe ser modularizado
+    expect(200).toBe(200);
+  });
+});
